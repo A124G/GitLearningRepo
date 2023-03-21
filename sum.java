51743d8731492f68cc9ea177bbc028c4744ee641
AJAY GUPTA <ajayamangupt786@gmail.com>
2023-03-21 17:39:49 +0530

    first commit

---
+++ sum.java
@@ -0,0 +1,11 @@
+class Sum{
+    private static void main(String[] args)  {
+        int a ;
+        int b;
+        int c;
+        c=a+b;
+        System.out.println(c);
+        
+        
+    }
+}
