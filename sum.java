69f2dc839fda3ee4d04a8a58745025c313505d40
AJAY GUPTA <ajayamangupt786@gmail.com>
2023-03-21 20:49:38 +0530

    update the file

--- sum.java
+++ sum.java
@@ -1,11 +1,11 @@
 class Sum{
     private static void main(String[] args)  {
-        int a ;
-        int b;
+        int a =5;
+        int b=5;
         int c;
         c=a+b;
         System.out.println(c);
-        
+        System.out.println(a);
         
     }
 }
